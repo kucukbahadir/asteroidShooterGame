dcffdc464538aff312c7a01a0e27447199cc8fcd
MikeHuijgen <mikeh162001@hotmail.com>
2025-02-12 14:25:03 +0100

    Create scripts for the button and to get the mouse position
    The buttons have an IIneractable interface. With the mouse you can click on the buttons too print out a log to test it out.

---
+++ Asteroid/Assets/Scripts/MouseController/MouseController.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    [SerializeField] private GameObject mouseControllerDebugPrefab;
+    [SerializeField] private bool useDebugPrefab;
+    [SerializeField] private LayerMask debugLayerMask;
+
+    private GameObject currentMouseControllerPrefab;
+    
+    private void Start()
+    {
+        currentMouseControllerPrefab = Instantiate(mouseControllerDebugPrefab);
+    }
+
+    private void Update ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(!Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue)) return;
+
+        OnUsingDebugPrefab(raycastHit);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!raycastHit.transform.TryGetComponent(out IInteractable interactable)) return;
+            interactable.Interact();   
+        }
+    }
+
+    private void OnUsingDebugPrefab(RaycastHit raycastHit)
+    {
+        if (!useDebugPrefab) return;
+        currentMouseControllerPrefab.transform.position = raycastHit.point;
+    }
+}
